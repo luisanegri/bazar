f2e650f2fead64cc0a0a30df1ff871ca9e16478f
Luisa Negri <luisa.negri@dytter.com>
2021-05-22 18:28:00 +0200

    initial commit and first screens

---
+++ app/screens/WelcomeScreen.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+import colors from '../styles/colors';
+
+function WelcomeScreen() {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require('../assets/background.jpg')}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require('../assets/favicon.png')} />
+        <Text>Sell what you don't need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: colors.secondary,
+  },
+});
+
+export default WelcomeScreen;
