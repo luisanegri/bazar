1c340fd531526f9172989c42d96c19c354fc79ad
Luisa Negri <luisa.negri@dytter.com>
2021-05-23 13:31:33 +0200

    implement listing screen

---
+++ app/screens/ListingScreen.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import Screen from '../components/Screen';
+import Card from '../components/Card';
+
+import colors from '../styles/colors';
+
+const listings = [
+  {
+    id: 1,
+    title: 'Pink jacket for sale',
+    price: 100,
+    image: require('../assets/jacket.jpg'),
+  },
+  {
+    id: 2,
+    title: 'Chair in great condition',
+    price: 200,
+    image: require('../assets/chair.jpg'),
+  },
+];
+
+export default function ListingScreen() {
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subtitle={'$' + item.price}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light,
+  },
+});
