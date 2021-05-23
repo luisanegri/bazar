bd3cac078c427f861b9380d9be9eed5ddc9d5539
Luisa Negri <luisa.negri@dytter.com>
2021-05-23 14:04:34 +0200

    create global styles file and default text style

--- app/components/AppTextInput.jsx
+++ app/components/AppTextInput.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import colors from '../styles/colors';
+import defaultStyles from '../styles/globalStyles';
 
 function AppTextInput({ icon, ...otherProps }) {
   return (
@@ -10,28 +10,23 @@ function AppTextInput({ icon, ...otherProps }) {
         <MaterialCommunityIcons
           name={icon}
           size={20}
-          color={colors.medium}
+          color={defaultStyles.colors.medium}
           style={styles.icon}
         />
       )}
-      <TextInput style={styles.textInput} {...otherProps} />
+      <TextInput style={defaultStyles.text} {...otherProps} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: 'row',
     width: '100%',
     padding: 15,
     marginVertical: 10,
-  },
-  textInput: {
-    fontSize: 18,
-    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-    color: colors.dark,
   },
   icon: {
     marginRight: 10,
